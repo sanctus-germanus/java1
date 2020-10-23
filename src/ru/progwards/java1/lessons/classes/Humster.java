da0c97318df221666694b870a66c91b5bfbded55
User <sanctus-germanus7@yandex.ru>
2020-10-23 11:55:50 +0300

    Project Start

---
+++ src/ru/progwards/java1/lessons/classes/Humster.java
@@ -0,0 +1,23 @@
+package ru.progwards.java1.lessons.classes;
+
+public class Humster extends Animal {
+
+    Humster(double weight) {
+        super(weight);
+    }
+
+    @Override
+    public AnimalKind getKind() {
+        return AnimalKind.HAMSTER;
+    }
+
+    @Override
+    public FoodKind getFoodKind() {
+        return FoodKind.CORN;
+    }
+
+    @Override
+    public double getFoodCoeff() {
+        return 0.03;
+    }
+}
